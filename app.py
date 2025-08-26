af0d7e30fbd10b18e9e3caaaea99326c5bb555fd
Fardin Alam <143535484+Fardin4135@users.noreply.github.com>
2025-08-26 19:54:53 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+def main():
+  a=5
+  b=10
+  print("Addition",add(a,b))
+  print("substraction",sub(a,b))
+
+if __name__ == "__main__":
+  main()
